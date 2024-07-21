0dc3e9e6a97bd5651e4637cb0e5feb657e7cd9f7
Abdisharif <sharifabdi735@gmail.com>
2024-07-21 13:54:01 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        name,
+        email
+      })
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then(data => {
+      document.body.innerHTML = `<h2>User created with id: ${data.id}</h2>`;
+      return data.id;
+    })
+    .catch(error => {
+      document.body.innerHTML = `<h2>Oops! Error: ${error.message}</h2>`;
+    });
+  }
